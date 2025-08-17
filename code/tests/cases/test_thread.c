44a4e1ab62e7d96d4586bddfc0b3e03ff611a47e
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-08-16 23:59:36 -0600

    add guts

---
+++ code/tests/cases/test_thread.c
@@ -0,0 +1,54 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Project: Fossil Logic
+ *
+ * This file is part of the Fossil Logic project, which aims to develop high-
+ * performance, cross-platform applications and libraries. The code contained
+ * herein is subject to the terms and conditions defined in the project license.
+ *
+ * Author: Michael Gene Brockus (Dreamer)
+ *
+ * Copyright (C) 2024 Fossil Logic. All rights reserved.
+ * -----------------------------------------------------------------------------
+ */
+#include <fossil/pizza/framework.h>
+#include "fossil/threads/framework.h"
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test Utilities
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// Setup steps for things like test fixtures and
+// mock objects are set here.
+// * * * * * * * * * * * * * * * * * * * * * * * *
+
+FOSSIL_TEST_SUITE(c_thread_fixture);
+
+FOSSIL_SETUP(c_thread_fixture) {
+    // Setup the test fixture
+}
+
+FOSSIL_TEARDOWN(c_thread_fixture) {
+    // Teardown the test fixture
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test Cases
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// The test cases below are provided as samples, inspired
+// by the Meson build system's approach of using test cases
+// as samples for library usage.
+// * * * * * * * * * * * * * * * * * * * * * * * *
+
+FOSSIL_TEST_CASE(c_thread_blaink) {
+    ASSUME_ITS_TRUE(1);
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test Pool
+// * * * * * * * * * * * * * * * * * * * * * * * *
+FOSSIL_TEST_GROUP(c_thread_tests) {    
+    FOSSIL_TEST_ADD(c_thread_fixture, c_thread_blaink);
+
+    FOSSIL_TEST_REGISTER(c_thread_fixture);
+} // end of tests
